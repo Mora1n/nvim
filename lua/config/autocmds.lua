7179b2840e8375b82c41af31255a87e727c35e68
morain <morain.fu@yuansuan.com>
2025-07-16 13:28:36 +0800

    group relative number autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,10 +1,14 @@
+local autocmd_group = vim.api.nvim_create_augroup('toggle_number', { clear = true })
+
 -- Toggle relative number
 vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
-  pattern = '*', 
+  group = autocmd_group,
+  pattern = '*',
   command = 'set norelativenumber',
 })
 vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
-  pattern = '*', 
+  group = autocmd_group,
+  pattern = '*',
   command = 'set relativenumber',
 })
 
